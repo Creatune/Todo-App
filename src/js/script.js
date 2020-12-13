4ecef69cd5a3661d462ca969eb8247d8ff9bdf35
Creatune <rjjoshixyz@gmail.com>
2020-12-13 15:16:36 +0530

    something

--- src/js/script.js
+++ src/js/script.js
@@ -4,16 +4,19 @@ function addTask() {
     let children = ul.children.length + 1
     li.setAttribute("id", "element" + children)
     let task = window.prompt("Enter task: ")
-    if(task != "") {
+    if(task != "" && task != null) {
         let checkbox = document.createElement("INPUT")
         let i = document.createElement('i')
         i.setAttribute("class", "fa fa-times closeTask")
+        i.setAttribute("id", "elementIcon" + children)
         checkbox.setAttribute("type", "checkbox")
         checkbox.setAttribute("class", "check")
         li.appendChild(checkbox)
         li.appendChild(document.createTextNode(task))
-        li.appendChild(i)
-        ul.appendChild(li)
+        li.appendChild(i);
+        ul.appendChild(li);
+        ("elementIcon" + children).addEventListener("click", document.getElementById("elementIcon" + children).innerHTML = "")
+
     }
     else {
         alert("Task field is empty. Try again")
